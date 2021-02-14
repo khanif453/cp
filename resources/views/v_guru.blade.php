6d274dff8be3cf9422d0f112f2990afb1b90d02e
khanif453 <72688462+khanif453@users.noreply.github.com>
2021-02-15 01:55:12 +0700

    commit cp

--- resources/views/v_guru.blade.php
+++ resources/views/v_guru.blade.php
@@ -2,11 +2,11 @@
 @section('title', 'Guru')
 
 @section('content')
-    @foreach ($guru as $data){
-        NIP : {{ $data['nip']}}<br>
-        Nama : {{ $data['nama']}}<br>
-        Mapel : {{ $data['mapel']}}<br>
+@foreach ($guru as $data)
+        NIP : {{ $data['nip'] }}<br>
+        Nama : {{ $data['nama'] }}<br>
+        Mapel : {{ $data['mapel'] }}<br>
         <br>
-    }
+    
     @endforeach
 @endsection 
