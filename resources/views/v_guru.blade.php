9c740ab902fd9f33ef30dac4a3cc6db9b626bf30
khanif453 <72688462+khanif453@users.noreply.github.com>
2021-02-15 04:41:58 +0700

    commit cp

--- resources/views/v_guru.blade.php
+++ resources/views/v_guru.blade.php
@@ -2,11 +2,34 @@
 @section('title', 'Guru')
 
 @section('content')
-@foreach ($guru as $data)
-        NIP : {{ $data['nip'] }}<br>
-        Nama : {{ $data['nama'] }}<br>
-        Mapel : {{ $data['mapel'] }}<br>
-        <br>
+
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <td>No</td>
+                <td>NIP</td>
+                <td>Nama</td>
+                <td>Mapel</td>
+                <td>Foto</td>
+            </tr>
+        </thead>
+        <tbody>
+        <?php $no = 1; ?>
+            @foreach ($guru as $data)
+            <tr>
+                <td> {{ $no++ }} </td>
+                <td> {{ $data->nip}} </td>
+                <td> {{ $data->nama_guru}} </td>
+                <td> {{ $data->mapel}} </td>
+                <td> <img src="{{ url('foto_guru/',$data->foto_guru) }}" width="100px"></td>
+                <td>
+                    <a href="" class="btn btn-sm btn-success">Detail</a>
+                    <a href="" class="btn btn-sm btn-primary">edit</a>
+                    <a href="" class="btn btn-sm btn-danger">Hapus</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
     
-    @endforeach
-@endsection 
+@endsection
